b1043813b5dd2ca6bd45b2ecbb0b7de055e42ac2
JazzPants <leighton.l14@gmail.com>
2021-12-02 19:03:48 +1100

    First Commit

---
+++ src/Cell.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Cell extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            color: props.values
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            color: '#333'
+        })
+    }
+
+    render(){
+        return (<div className='cell'
+        style={{backgroundColor: this.state.color}}
+        onClick={this.handleClick}></div>)
+    }
+}
+
+export default Cell
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import Cell from './Cell'
 
 export default class Matrix extends Component {
   
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    return vals.map((val, i) => <Cell key={i} value={val} className="cell"></Cell>) // replace me and render a cell component instead!
   }
   
   genMatrix = () => {
-    return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+    return this.props.values.map((rowVals,i) => <div className="row" key={i}>{this.genRow(rowVals)}</div>)
   }
   
   render() {
@@ -19,3 +20,18 @@ export default class Matrix extends Component {
   }
   
 }
+
+Matrix.defaultProps = {
+  values:[['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+    ['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+    ['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+    ['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+    ['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+    ['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+    ['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+    ['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+    ['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+    ['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00']]
+  }
+//or use immediately invoked function to make it 10x10 so you only have to write array once
+
